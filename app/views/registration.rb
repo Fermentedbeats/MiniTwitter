c2be2c36af7d8023ea991baa0c170572d9892d87
CJ Johnson & jayceef <pair+fermentedbeats.jayceef@devbootcamp.com>
2015-01-16 15:00:50 -0800

    Need to complete homepage sidebar

--- app/views/registration.rb
+++ app/views/registration.rb
@@ -1,3 +1,16 @@
+
+<div class="jumbotron">
+  <div class="container">
+    <h1>MiniTwitter</h1>
+    <h4>The New Way to Connect</h4>
+  </div>
+</div>
+
+<div class="stage">
+  <img src="http://clients.austereo.com.au/vanity/fifa14/assets/img/twitter-logo.png" class="ball"></figure>
+</div>
+
+
  <form action='/registration' method='post'>
  <p>Welcome to MiniTwitter</p>
   <input type='text' name='user[handle]' placeholder='handle'/>
